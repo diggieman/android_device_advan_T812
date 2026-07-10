26437c77accd2b0eb805b892f3005631c1f993cd
diggieman <digaholez@proton.me>
2026-07-10 14:52:07 +0700

    T812: Use common thermal HAL

--- device.mk
+++ device.mk
@@ -278,7 +278,10 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl:64
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # Updatable APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
@@ -290,7 +293,7 @@ PRODUCT_PACKAGES += \
 # Wi-Fi
 $(call soong_config_set_bool,wpa_supplicant_8,board_wlan_mediatek_stability,true)
 
-PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \y
     android.hardware.wifi-service \
     hostapd \
     wpa_supplicant
